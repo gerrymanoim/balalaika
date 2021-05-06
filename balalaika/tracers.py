e9bafb864f56cc4f25e6c226bd4409a662a3692a
Gerry Manoim <gerry.manoim@twosigma.com>
2021-05-06 02:07:40 -0400

    Add some mermaid sequence diagram support

--- balalaika/tracers.py
+++ balalaika/tracers.py
@@ -1,5 +1,8 @@
+import re
+from weakref import WeakKeyDictionary
+
 import trio
-from rich import print
+from rich import print as rprint
 
 
 class BasicTracer(trio.abc.Instrument):
@@ -12,7 +15,7 @@ class BasicTracer(trio.abc.Instrument):
 
     def _print(self, msg, task=""):
         padding = " " * 4 * self.indent
-        print(f"{padding}{msg}: {task}")
+        rprint(f"{padding}{msg}: {repr(task)}")
 
     def task_spawned(self, task):
         self._print("### new task spawned", task)
@@ -45,3 +48,39 @@ class BasicTracer(trio.abc.Instrument):
     def after_run(self):
         self.indent -= 1
         self._print("!!! run finished")
+
+
+class MermaidSequenceDiagramTracer(trio.abc.Instrument):
+    def __init__(self) -> None:
+        self._link = None
+        self.out = ""
+        self._arrow = "->>"
+        self._spaces = "    "
+        self._task_name_storage = WeakKeyDictionary()
+
+    def _clean_name(self, task: str) -> str:
+        """
+        Hacky cleaning to comply with mermaid syntax
+        """
+        if task in self._task_name_storage:
+            return self._task_name_storage[task]
+        # we want to map different closures to different names
+        uuid = str(id(task)) if "<locals>" in task.name else ""
+        clean_name = re.sub(r"[^a-zA-Z0-9\.\_]", "*", task.name) + uuid
+        self._task_name_storage[task] = clean_name
+        return clean_name
+
+    def _advance_link(self, task):
+        task_name = self._clean_name(task)
+        if self._link:
+            self.out += "\n"
+            self.out += f"{self._spaces}{self._link}{self._arrow}{task_name}: "
+        self._link = task_name
+
+    def before_task_step(self, task):
+        self._advance_link(task)
+
+    def after_run(self):
+        out = "sequenceDiagram"
+        out += self.out + "."  # not sure why we need this dot
+        print(out)
--- examples/scratch.py
+++ examples/scratch.py
@@ -1,8 +1,8 @@
-from balalaika.tracers import BasicTracer
+from balalaika.tracers import BasicTracer, MermaidSequenceDiagramTracer
 import trio
 
 
 trio.run(
     trio.open_tcp_stream, "github.com", 443,
-    instruments=[BasicTracer()],
+    instruments=[MermaidSequenceDiagramTracer()],
 )
